0e1c52f55a0dcb9dd4b29f98ff8543d8d584b63e
Saman Sadeghyan <samronsadeghyan@gmail.com>
2016-03-11 21:42:44 +0330

    Added files via upload

---
+++ Source.c
@@ -0,0 +1,13 @@
+extern int f(int,int *, int *);
+
+int main() {
+	
+	int x[] = { 1, 2, 3, 4, 5, 6 };
+	int y[] = { 1, 2, 3, 4, 5, 6 };
+	int n = 6;
+
+	f(n, x, y);
+	//printf("Hello, World! \d"+z);
+	
+	return 0;
+}
